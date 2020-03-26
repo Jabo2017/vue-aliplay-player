b8b8990ab5d19505e7ebe23a65feb86fbbe59d61
huangjb <huangjb8379>
2020-03-26 18:05:24 +0800

    更新

--- packages/vue-aliplay-player/index.js
+++ packages/vue-aliplay-player/index.js
@@ -1,5 +1,9 @@
 import vueAliplayer from './src/player.vue'
-vueAliplayer.install = function(vue) {
+
+ /**
+  * 预览做全局组件使用，按需加载组件可以不需要这个install
+  */
+vueAliplayer.install = function(vue) { 
 	Vue.component(vueAliplayer.name, vueAliplayer)
 }
 export default vueAliplayer
